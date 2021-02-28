4bdf1cf5043946a6b89de92a7f366cef0b3169ab
YastMe <64034487+YastMe@users.noreply.github.com>
2021-02-28 16:34:14 +0100

    Creación de ventana

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import tkinter as tk
 
 def winconfig():
     principal.title("Ficha Python")
-    principal.geometry("500x300+200+100")
-    principal.resizable(width="False", height="False")
+    principal.geometry("1920x1080+200+100")
+    principal.resizable(width="True", height="True")
 
 principal = tk.Tk()
 winconfig()
