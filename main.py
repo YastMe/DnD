f2d7312190e9ae78871249f10fca2553ee97f711
YastMe <64034487+YastMe@users.noreply.github.com>
2021-02-28 16:30:57 +0100

    Creación de ventana

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import openpyxl
 import tkinter as tk
+
+def winconfig():
+    principal.title("Ficha Python")
+    principal.geometry("500x300+200+100")
+    principal.resizable(width="False", height="False")
+
+principal = tk.Tk()
+winconfig()
+
+principal.mainloop()
